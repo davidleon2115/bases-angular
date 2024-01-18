c37d872461eb0bcd5f130c1e753ece6a1b42be70
David Leon <leon95024@gmail.com>
2024-01-18 01:04:50 -0500

    Heroes second part

--- src/app/heroes/hero/hero.component.ts
+++ src/app/heroes/hero/hero.component.ts
@@ -8,4 +8,29 @@ import { Component } from '@angular/core';
 export class HeroComponent {
   public name: string = 'ironman';
   public age:  number = 45;
+
+  get capitalizedName():string {
+    return this.name.charAt(0).toUpperCase() + this.name.slice(1)
+  }
+
+  getHeroDescription():string{
+    return `${this.capitalizedName} - ${this.age}`
+  }
+
+  changeHero():void {
+
+    if (this.name === "ironman") {
+      this.name = "spiderman";
+    }else{
+      this.name = "ironman"
+    } 
+  }
+    
+  changeAge():void {
+    if (this.age === 45){
+      this.age = 32;
+    }else{
+      this.age = 45;
+    }
+  }
 }
