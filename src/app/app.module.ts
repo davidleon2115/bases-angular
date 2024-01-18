eab87c9d2a94636cba1f2b8417dca15766853de1
David Leon <leon95024@gmail.com>
2024-01-17 21:47:57 -0500

    Add heroes component files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CounterComponent } from './counter/counter.component';
+import { HeroComponent } from './heroes/hero/hero.component';
+import { ListComponent } from './heroes/list/list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CounterComponent
+    CounterComponent,
+    HeroComponent,
+    ListComponent
   ],
   imports: [
     BrowserModule,
